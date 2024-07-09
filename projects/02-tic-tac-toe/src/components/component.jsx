170ba792881491ecb7ce96568286dcf12fd00a0c
Xavi Serrano <xserrano2001@gmail.com>
2024-07-09 10:42:55 +0200

    Add standard Lint

--- projects/02-tic-tac-toe/src/components/component.jsx
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react'
-const Component = () => {
-  const [value, setValue] = useState(false)
-
-  useEffect(() => {
-    // como mínimo, se ejecuta una vez
-    console.log('El codigo a ejecutar')
-  })
-}
